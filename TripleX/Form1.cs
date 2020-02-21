efe36cd67171e4d97f5ceaddbb69e9e243887fce
Dmitrii <visparx@gmail.com>
2020-02-21 17:06:19 +0300

    Начальный функционал
    Сделаны кнопки, при нажатии на которые по очереди будет записываться крестик или нолик. На нажатые кнопки больше нажимать нельзя

--- TripleX/Form1.cs
+++ TripleX/Form1.cs
@@ -12,9 +12,25 @@ namespace TripleX
 {
     public partial class Form1 : Form
     {
+        bool xTurn = true;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button senderB = (Button)sender;
+            if (xTurn)
+            {
+                senderB.Text = "X";
+            }
+            else
+            {
+                senderB.Text = "O";
+            }
+            xTurn = !xTurn;
+            senderB.Enabled = false;
         }
     }
 }
